41d897cf9ee80025159f8c053862c2f085c67870
famousfamous <writefamous@gmail.com>
2025-05-22 12:16:33 +0100

    8 args

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  const line = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) {
+    console.log(line);
+  }
+}
+
