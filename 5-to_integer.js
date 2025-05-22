1f1f8a529b02a160ee6e3deab1463d11933146fe
famousfamous <writefamous@gmail.com>
2025-05-22 12:08:18 +0100

    5 args

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+const num = Number(args[0]);
+
+if (!isNaN(num) && args[0] !== undefined && args[0].trim() !== '') {
+  console.log(`My number: ${parseInt(num)}`);
+} else {
+  console.log("Not a number");
+}
+
